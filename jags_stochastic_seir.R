65e46a1f91409f4981b2a35fc16197c6ce584662
graham gibson <gcgibson@umass.edu>
2020-04-02 08:43:51 -0600

    update

--- jags_stochastic_seir.R
+++ jags_stochastic_seir.R
@@ -32,9 +32,9 @@ plot(out$I)
 model <- "
 model {
    
-    beta ~ dnorm(.95,100000000)
-    gamma ~ dnorm(1/3.5,100000000)
-    sigma ~ dnorm(1/3.67,100000000)
+    beta ~ dnorm(.95,10000)
+    gamma ~ dnorm(1/3.5,10000)
+    sigma ~ dnorm(1/3.67,10000)
 
     #E_prop_init ~ dbeta(100000*1e-5,100000*(1-1e-5))
     I_prop_init ~ dbeta(100000*1e-5,100000*(1-1e-5))
@@ -43,24 +43,32 @@ model {
     E[1] <- 0
     I[1] <- round(n_sample*I_prop_init)
     R[1] <- 0
-    delta_t ~ dbeta(10*.25,10*(1-.25))
+    delta_t ~ dbeta(100*.1,100*(1-.1))
     for (i in 2:n_obs){
     
-        delta_n_se[i] ~ dbinom(1-exp(-beta*I[i-1]/n_sample*delta_t),S[i-1])
-        delta_n_ei[i] ~ dbinom(1-exp(-sigma*delta_t),E[i-1])
-        delta_n_ir[i] ~ dbinom(1-exp(-gamma*delta_t),I[i-1])
-        S[i] <- S[i-1] - delta_n_se[i]
-        E[i] <- E[i-1] + delta_n_se[i] -delta_n_ei[i] 
-        I[i] <- I[i-1] + delta_n_ei[i] - delta_n_ir[i]
-        R[i] <- R[i-1] + delta_n_ir[i]
+        #delta_n_se[i] ~ dbinom(1-exp(-beta*I[i-1]/n_sample*delta_t),S[i-1])
+        #delta_n_ei[i] ~ dbinom(1-exp(-sigma*delta_t),E[i-1])
+        #delta_n_ir[i] ~ dbinom(1-exp(-gamma*delta_t),I[i-1])
+        
+        p_se[i] <- 1-exp(-beta*I[i-1]/n_sample*delta_t)
+        p_ei[i] <- 1-exp(-sigma*delta_t)
+        p_ir[i] <- 1-exp(-gamma*delta_t)
+        delta_n_se[i] ~ dbeta(10*p_se[i],10*(1-p_se[i]))
+        delta_n_ei[i] ~ dbeta(10*p_ei[i],10*(1-p_ei[i]))
+        delta_n_ir[i] ~ dbeta(10*p_ir[i],10*(1-p_ir[i]))
+        
+        S[i] <- S[i-1] - delta_n_se[i]*S[i-1]
+        E[i] <- E[i-1] + delta_n_se[i]*S[i-1] -delta_n_ei[i]*E[i-1]
+        I[i] <- I[i-1] + delta_n_ei[i]*E[i-1] - delta_n_ir[i]*I[i-1]
+        R[i] <- R[i-1] + delta_n_ir[i]*I[i-1]
     }
 
     for (i in 1:n_obs){
-      y[i] ~ dnorm(I[i],1000000)
+      y[i] ~ dnorm(I[i],1)
     }
-    #for (j in 14:d_length){
-    #    d[j] ~ dnorm(I[j-13]*.0134 ,1000)
-   # }
+   for (j in 14:d_length){
+        d[j] ~ dnorm(I[j-13]*.0134 ,1000)
+    }
 
 }"
 state_testing_data <- read.csv("/Users/gcgibson/Downloads/states-daily.csv")
@@ -85,13 +93,13 @@ state_testing_data_ny$death[is.na(state_testing_data_ny$death)] <- 0
 # subset to last 20 rows
 nrow_df <- nrow(state_testing_data_ny)
 state_testing_data_ny_subset <- state_testing_data_ny[tail(1:nrow_df,20),]
+h <- 0
 
-
-stan_d = list(n_obs = length(state_testing_data_ny_subset$positive)+7*8,
+stan_d = list(n_obs = length(state_testing_data_ny_subset$positive)+h,
               n_params = 3,
               n_difeq = 4,
               n_sample = 10^6,
-              y = c(state_testing_data_ny_subset$positive,rep(NA,7*8)),
+              y = c(state_testing_data_ny_subset$positive,rep(NA,h)),
               #y=rep(NA,length(state_testing_data_ny_subset$positive)+7*4),
               d = state_testing_data_ny_subset$death,
               d_length = length(state_testing_data_ny_subset$death))
@@ -99,8 +107,13 @@ stan_d = list(n_obs = length(state_testing_data_ny_subset$positive)+7*8,
 
 jgs <- jags.model(file = textConnection(model), data = stan_d, n.adapt = 1000)
 update(jgs, 1000)
-out_jags <- jags.samples(jgs, c('I','beta','gamma'), 3000, 3)
-plot(rowMeans(out_jags$I))
-lines(state_testing_data_ny_subset$positive,col='red')
+out_jags <- jags.samples(jgs, c('I','beta','gamma','y'), 3000, 3)
+#plot(rowMeans(out_jags$I))
+#lines(state_testing_data_ny_subset$positive,col='red')
+library(ggplot2)
+data_for_plot <- data.frame(x=rep(1:dim(out_jags$I)[1],dim(out_jags$I)[2]),
+                            y=c(out_jags$I),group=rep(1:dim(out_jags$I)[2],each=dim(out_jags$I)[1]))
+
+ggplot(data_for_plot,aes(x=x,y=y,group=group)) + geom_line(alpha=.1) + theme_bw() + geom_line(data=data.frame(x=1:length(state_testing_data_ny_subset$positive),y=state_testing_data_ny_subset$positive),aes(x=x,y=y,group=1,col='observed'))
 hist(out_jags$beta)
 hist(out_jags$gamma)
